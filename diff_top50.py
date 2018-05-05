970f1bca941bf6bf7a9c17bccc78e316e61f62dd
Steven Canning <spcanning@gmail.com>
2018-05-05 13:22:04 -0700

    Improved formatting when printing comparison top50

--- diff_top50.py
+++ diff_top50.py
@@ -13,23 +13,30 @@ def main(old_file, new_file):
 
     old_top50_gameids = [x[1] for x in old_top50]
 
+    name_width = max([len(x[0]) for x in new_top50])
+    format_string = u"{:2} ({:3}). {:" + str(name_width) + u"} {:3} {:5.3f} {:5.3f}"
+
     for index, game_info in enumerate(new_top50):
         try:
             old_index = old_top50_gameids.index(game_info[1])
         except ValueError:
             old_index = -1
 
-        detail_string = str(index + 1) + ' ('
         if old_index > -1:
             diff = old_index - index
             if diff > -1:
-                detail_string += '+' + str(diff) + '). '
+                diff_string = '+' + str(diff)
             else:
-                detail_string += str(diff) + '). '
+                diff_string = str(diff)
         else:
-            detail_string += 'new). '
+            diff_string = 'new'
 
-        detail_string += '%s %d %.3f %.3f' % (game_info[0], game_info[2], game_info[3], game_info[4])
+        detail_string = format_string.format(index + 1,
+                diff_string,
+                game_info[0],
+                game_info[2],
+                game_info[3],
+                game_info[4])
         print detail_string
 
 if __name__ == '__main__':
